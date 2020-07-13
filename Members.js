68b613d17ce7e64f04735ed53fc9702318a1a281
Sadhana <me@sadhana.com>
2020-07-13 10:22:34 +0530

    i

---
+++ Members.js
@@ -0,0 +1,21 @@
+const members = [
+  {
+    id: 1,
+    name: "sss",
+    email: "sss@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "ddd",
+    email: "ddd@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "bbb",
+    email: "bbb@gmail.com",
+    status: "active",
+  },
+];
+module.exports = members;
